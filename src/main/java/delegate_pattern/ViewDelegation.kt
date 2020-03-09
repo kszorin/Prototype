7a7f640c5dd26ffa3871c2339741b32cd0fe2f8b
kszorin <kszorin@yandex.ru>
2020-03-09 22:20:00 +0700

    Add file from delegate pattern master-class Mikhail

---
+++ src/main/java/delegate_pattern/ViewDelegation.kt
@@ -0,0 +1,64 @@
+package delegate_pattern
+
+import delegate_pattern.PersonViewModule.providePersonViewDelegate
+import delegate_pattern.PersonViewModule.provideSender
+
+fun main(args: Array<String>) {
+    OmgFragment(
+        provideSender(
+            providePersonViewDelegate()
+        )
+    )
+}
+
+object PersonViewModule {
+
+    fun providePersonViewDelegate(): PersonViewDelegate =
+        PersonFragmentDelegate()
+
+    fun provideSender(personViewDelegate: PersonViewDelegate): SenderView =
+        SenderFragment(personViewDelegate)
+}
+
+interface OmgView
+
+class OmgFragment(senderView: SenderView) : OmgView {
+
+    init {
+        senderView.imSender()
+    }
+}
+
+interface PersonViewDelegate {
+
+    fun printPerson(personName: String)
+}
+
+class PersonPresenterDelegate(val view: PersonViewDelegate)
+
+class PersonFragmentDelegate : PersonViewDelegate {
+
+    override fun printPerson(personName: String) {
+        println("My name is $personName")
+    }
+}
+
+interface SenderView {
+
+    fun imSender()
+}
+
+class SendPresenter(val view: SenderView)
+
+class SenderFragment(val personViewDelegate: PersonViewDelegate) :
+    SenderView {
+
+    private companion object {
+        const val NAME = "Boris"
+    }
+
+    override fun imSender() {
+        personViewDelegate.printPerson(NAME)
+        println("I'm Sender!")
+    }
+}
