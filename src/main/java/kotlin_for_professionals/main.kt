5bade88589c025dd42ec7246ac3cd344ddf5064e
kszorin <kszorin@yandex.ru>
2020-05-19 21:35:06 +0700

    chapter-2 "Рог единорога" и "Волшебное зеркало"

---
+++ src/main/java/kotlin_for_professionals/main.kt
@@ -0,0 +1,12 @@
+package kotlin_for_professionals
+
+val playerName = "Estragon"
+var hasSteed: Boolean = false
+val tavernName = "Unicorn horn"
+val innkeeperName = "Lui"
+val coinsAmount = 10
+
+fun main(args: Array<String>) {
+    println(playerName)
+    println(playerName.reversed())
+}
