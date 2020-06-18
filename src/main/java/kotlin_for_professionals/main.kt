48bb6c8833344137b21d6a91d2bf2476c6b489dd
kszorin <kszorin@yandex.ru>
2020-06-18 20:52:56 +0700

    chapter-3 Код перед заданиями главы

--- src/main/java/kotlin_for_professionals/main.kt
+++ src/main/java/kotlin_for_professionals/main.kt
@@ -1,12 +1,29 @@
 package kotlin_for_professionals
 
-val playerName = "Estragon"
-var hasSteed: Boolean = false
-val tavernName = "Unicorn horn"
-val innkeeperName = "Lui"
-val coinsAmount = 10
-
 fun main(args: Array<String>) {
-    println(playerName)
-    println(playerName.reversed())
+    val name = "Madrigal"
+
+    val healthPoints = 89
+    val blessed = true
+
+    val immortal = false
+
+    val auraVisible = blessed && healthPoints > 50 || immortal
+    val auraColor = if (auraVisible) "GREEN" else "NONE"
+    println("(Aura: $auraColor) (Blessed: ${if (blessed) "YES" else "NO"})")
+
+    val healthStatus = when (healthPoints) {
+        100 -> "is excelent condition!"
+        in 90..99 -> "has a few scratches."
+        in 75..89 -> if (blessed) {
+            "has some minor wounds, but is healing quite quickly!"
+        } else {
+            "has some minor wounds."
+        }
+        in 15..74 -> "looks pretty hurt."
+        else -> "is in awful condition!"
+    }
+
+    println("$name $healthStatus")
+
 }
