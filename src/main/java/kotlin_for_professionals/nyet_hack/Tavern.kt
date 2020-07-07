800f29a462d3e5de893c7eab20a7a9d5f6d71a4c
kszorin <kszorin@yandex.ru>
2020-07-07 13:11:06 +0700

    chapter-6 Проверка на null

--- src/main/java/kotlin_for_professionals/nyet_hack/Tavern.kt
+++ src/main/java/kotlin_for_professionals/nyet_hack/Tavern.kt
@@ -1,6 +1,12 @@
 package kotlin_for_professionals.nyet_hack
 
 fun main(args: Array<String>) {
-    val beverage = readLine()!!.capitalize()
+    var beverage = readLine()
+    if (beverage != null) {
+        beverage = beverage.capitalize()
+    } else {
+        println("I can do it without NPE")
+    }
+
     println(beverage)
 }
