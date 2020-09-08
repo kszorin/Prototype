307a15936b8672f4a5dbfaf8fe45cef2d5b61801
kszorin <kszorin@yandex.ru>
2020-09-08 21:00:45 +0700

    chapter-13 Порядок инициализации
    Пример демонстрирует важность порядка инициализации полей в классе.

---
+++ src/main/java/kotlinForProfessionals/tasks/Chapter13_2.kt
@@ -0,0 +1,18 @@
+package kotlinForProfessionals.tasks
+
+fun main() {
+
+    val npeGenerator = NPEGenerator()
+}
+
+class NPEGenerator {
+
+    var a: IntArray
+    val b = initB()
+
+    init {
+       a = intArrayOf(5, 5, 6)
+    }
+
+    private fun initB() = a[0]
+}
