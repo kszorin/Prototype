dcb0f49d140bbef6f18b06d73aa3af2c80cd3dd1
zorin <k.zorin@cft.ru>
2021-05-05 21:02:56 +0700

    chapter-19 Сделал профелирования для двух вариантов вывода чисел Фибоначчи

--- src/main/java/kotlinForProfessionals/tasks/chapter19/Chapter19_3.kt
+++ src/main/java/kotlinForProfessionals/tasks/chapter19/Chapter19_3.kt
@@ -1,12 +1,41 @@
 package kotlinForProfessionals.tasks.chapter19
 
-fun main() {
+import kotlin.system.measureNanoTime
 
+fun main() {
+	val bySequance = measureNanoTime {
+		printFibBySequance()
+	}
+
+	val byCommon = measureNanoTime {
+		printFibByCommon()
+	}
+
+	println("FibBySequance = $bySequance")
+	println("FibByCommon = $byCommon")
+}
+
+fun printFibBySequance() {
 	generateSequence(Pair(0, 1)) { value ->
 		Pair(value.second, value.first + value.second)
 	}
 		.map { it.second }
 		.takeWhile { it > 0 }
 		.forEach { println(it) }
+}
+
+fun printFibByCommon() {
+	var a = 0
+	var b = 1
+	var result = b
+	println(result)
+	while (result >= 0) {
+		result = a + b
+		if (result >= 0) {
+			println(result)
+		}
+		a = b
+		b = result
+	}
 }
 
