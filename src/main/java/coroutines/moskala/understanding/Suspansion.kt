522a029c8c0e9fad2c3b5dfe0e8a0ee200b3ce99
kszorin <kszorin@yandex.ru>
2022-08-11 19:28:12 +0700

    Создаём задержку через executor вместо отдельного потока
    Chapter "How does suspension work?"

--- src/main/java/coroutines/moskala/understanding/Suspansion.kt
+++ src/main/java/coroutines/moskala/understanding/Suspansion.kt
@@ -1,23 +1,26 @@
 package coroutines.moskala.understanding
 
-import kotlin.concurrent.thread
-import kotlin.coroutines.Continuation
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
 
+private val executor = Executors.newSingleThreadScheduledExecutor { runnable ->
+    Thread(runnable, "scheduler").apply { isDaemon = true }
+}
+
 suspend fun main() {
     println("${Thread.currentThread().name} before")
-    suspendCoroutine<Unit> {cont ->
-        continueAfterSecond(cont)
-    }
+    customDelay(2000)
     println("${Thread.currentThread().name} after")
 }
 
-private fun continueAfterSecond(continuation: Continuation<Unit>) {
-    thread {
-        println("${Thread.currentThread().name} before sleep")
-        Thread.sleep(2000)
-        println("${Thread.currentThread().name} after sleep")
-        continuation.resume(Unit)
+suspend fun customDelay(millis: Long) {
+    suspendCoroutine<Unit> { cont ->
+        executor.schedule(
+            { cont.resume(Unit) },
+            millis,
+            TimeUnit.MILLISECONDS
+        )
     }
 }
