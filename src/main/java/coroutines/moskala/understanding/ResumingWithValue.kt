68b87bb0ee5444f5b0156fa0b7d649a68255bfc0
kszorin <kszorin@yandex.ru>
2022-08-12 11:37:26 +0700

    Добавил возврат исключения

--- src/main/java/coroutines/moskala/understanding/ResumingWithValue.kt
+++ src/main/java/coroutines/moskala/understanding/ResumingWithValue.kt
@@ -1,24 +1,41 @@
 package coroutines.moskala.understanding
 
 import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
 import kotlin.coroutines.suspendCoroutine
 
 suspend fun main() {
     println("[${Thread.currentThread().name}] main() before")
-    val user: String = requestUser()
-    println("[${Thread.currentThread().name}] User = $user")
+    try {
+        val user: String = requestUser()
+        println("[${Thread.currentThread().name}] User = $user")
+    } catch (ex: java.lang.Exception) {
+        println(ex)
+    }
+
     println("[${Thread.currentThread().name}] main() after")
 }
 
 private suspend fun requestUser(): String =
     suspendCoroutine { cont ->
-        requestUserApi { cont.resume(it) }
+        try {
+            requestUserApi(
+                onSuccess = cont::resume,
+                onError = cont::resumeWithException
+            )
+        } catch (ex: Exception) {
+            cont.resumeWithException(ex)
+        }
     }
 
-private fun requestUserApi(block: (String) -> Unit) {
+private fun requestUserApi(
+    onSuccess: (String) -> Unit,
+    onError: (Exception) -> Unit,
+) {
     println("[${Thread.currentThread().name}] requestUser() before")
     Thread.sleep(2000)
-    val user = "Vasya Pupkin"
-    block(user)
+//    val user: String? = "Vasya Pupkin"
+    val user: String? = null
+    user?.let { onSuccess(user) } ?: onError(Exception("Can't get user!"))
     println("[${Thread.currentThread().name}] requestUser() after")
 }
